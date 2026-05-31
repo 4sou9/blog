0afe9b29758570f8b4e8b0a43622785869531b22
4sou9 <4sou99@gmail.com>
2026-06-01 05:56:37 +0900

    remove font config

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,28 +9,4 @@ export default defineConfig({
 	site: 'https://4sou9.github.io',
 	base: '/blog',
 	integrations: [mdx(), sitemap()],
-	fonts: [
-		{
-			provider: fontProviders.local(),
-			name: 'Atkinson',
-			cssVariable: '--font-atkinson',
-			fallbacks: ['sans-serif'],
-			options: {
-				variants: [
-					{
-						src: ['./src/assets/fonts/atkinson-regular.woff'],
-						weight: 400,
-						style: 'normal',
-						display: 'swap',
-					},
-					{
-						src: ['./src/assets/fonts/atkinson-bold.woff'],
-						weight: 700,
-						style: 'normal',
-						display: 'swap',
-					},
-				],
-			},
-		},
-	],
 });
